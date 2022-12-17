ab73b1e52c2f3eceec4cb2d0a15e583848e9786f
IdNotFounded <mashenka8m@gmail.com>
2022-12-17 15:13:59 +0300

    changes commit

---
+++ untitled/src/server.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+class Server {
+    public static void main(String[] args) {
+
+        try (ServerSocket server = new ServerSocket(8000)) {
+
+        System.out.println("Server started!");
+
+        while (true)
+            try (Socket socket = server.accept();
+
+                 BufferedWriter writer = new BufferedWriter(
+                         new OutputStreamWriter(
+                                 socket.getOutputStream()));
+                 BufferedReader reader = new BufferedReader(
+                         new InputStreamReader(
+                                 socket.getInputStream()))) {
+
+                String request = reader.readLine();
+                writer.write("Test");
+                writer.newLine();
+                writer.flush();
+
+                System.out.println("Client");
+                System.out.println(request);
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+        } catch (IOException e) {
+                throw new RuntimeException(e);
+        }
+    }
+}
